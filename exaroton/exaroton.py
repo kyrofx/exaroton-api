5a6fbf5d8036420a08c078ab95463e6d88aed5db
kyro <eddie.gu26@bcp.org>
2023-01-30 12:28:53 -0800

    Little change.

--- exaroton/exaroton.py
+++ exaroton/exaroton.py
@@ -22,40 +22,9 @@ class Exaroton:
         self._session = requests.Session()
         self._session.headers.update({"Authorization": f"Bearer {token}"})
 
-    def _make_request(self, path: str, method: str = "get", **kwargs):
-        """Make HTTP Requests against the API
 
-        Parameters:
-            ``path`` (``str``): The API calls' path for the request.
-            ``method`` (``str``, optional): HTTP Method (GET, POST, PUT, etc). Defaults to GET.
-            ``**kwargs``: Additional arguments passed to the API call.
 
-        Returns:
-            JSON serialized data
-        """
-        req = self._session.request(method, f"{self._host}/{path}", **kwargs)
-        # TODO Error Handling
-        return req.json()
-
-    def get_account(self) -> types.Account:
-        """Get information about the authenticated Account
-
-        Returns:
-            ``types.Account``: Your Account
-        """
-        _data = self._make_request("account")["data"]
-        return types.Account(**_data)
-
-    def get_servers(self) -> types.List:
-        """Get a list of servers on your account
-
-        Returns:
-            ``types.List``: List of ``types.Server`` objects
-        """
-        _data = self._make_request("servers")["data"]
-        return types.List(types.Server(**data) for data in _data)
-
-    def get_server(self, id: str) -> types.Server:
+    def get_server(self) -> types.Server:
         """Get a specific Server on your account
 
         Args:
@@ -64,34 +33,10 @@ class Exaroton:
         Returns:
             ``types.Server``: The Server
         """
-        _data = self._make_request(f"servers/{id}")["data"]
+        _data = self._make_request(f"servers/Pn8J6Yy0TWUqflP6")["data"]
         return types.Server(**_data)
 
-    def get_server_logs(self, id: str) -> str:
-        """Retrieve logs of the specified server
-
-        Args:
-            ``id`` (``str``): The ID of the server
-
-        Returns:
-            ``str``: The current log file in its entirety.
-        """
-        _data = self._make_request(f"servers/{id}/logs")["data"]["content"]
-        return _data
-
-    def upload_logs(self, id: str) -> types.Logs:
-        """Upload logs to https://mclo.gs
-
-        Args:
-            ``id`` (``str``): The ID of the server
-
-        Returns:
-            ``types.Logs``: Identifier and URLS to the uploaded log
-        """
-        _data = self._make_request(f"servers/{id}/logs/share")["data"]
-        return types.Logs(**_data)
-
-    def get_server_ram(self, id: str) -> int:
+    def get_server_ram(self) -> int:
         """Get the RAM of a Server
 
         Args:
@@ -100,25 +45,10 @@ class Exaroton:
         Returns:
             ``int``: Currently set RAM in Gigabytes
         """
-        _data = self._make_request(f"servers/{id}/options/ram")["data"]["ram"]
+        _data = self._make_request(f"servers/Pn8J6Yy0TWUqflP6/options/ram")["data"]["ram"]
         return _data
 
-    def set_server_ram(self, id: str, ram: int) -> int:
-        """Set a new amount of RAM to be used by the specified server
-
-        Args:
-            ``id`` (``str``): The ID of the server
-            ``ram`` (``int``): RAM in Gigabyte
-
-        Returns:
-            ``int``: Newly set RAM in Gigabytes
-        """
-        _data = self._make_request(
-            f"servers/{id}/options/ram", "post", json={"ram": ram}
-        )["data"]["ram"]
-        return _data
-
-    def start(self, id: str) -> str:
+    def start(self) -> str:
         """Start the Server
 
         Args:
@@ -127,10 +57,10 @@ class Exaroton:
         Returns:
             ``str``: "Hello, world!"
         """
-        _data = self._make_request(f"servers/{id}/start")
+        _data = self._make_request(f"servers/Pn8J6Yy0TWUqflP6/start")
         return _data
 
-    def stop(self, id: str) -> str:
+    def stop(self) -> str:
         """Stop the Server
 
         Args:
@@ -139,10 +69,10 @@ class Exaroton:
         Returns:
             ``str``: "Hello, world!"
         """
-        _data = self._make_request(f"servers/{id}/stop")
+        _data = self._make_request(f"servers/Pn8J6Yy0TWUqflP6/stop")
         return _data
 
-    def restart(self, id: str) -> str:
+    def restart(self) -> str:
         """Restart the Server
 
         Args:
@@ -151,37 +81,11 @@ class Exaroton:
         Returns:
             ``str``: "Hello, world!"
         """
-        _data = self._make_request(f"servers/{id}/restart")
+        _data = self._make_request(f"servers/Pn8J6Yy0TWUqflP6/restart")
         return _data
 
-    def command(self, id: str, command: str) -> str:
-        """Send a Command to the Server
 
-        Args:
-            ``id`` (``str``): The ID of the server
-            ``command`` (``str``): The command (`say Hello World`)
-
-        Returns:
-            ``str``: "Hello, world!"
-        """
-        _data = self._make_request(
-            f"servers/{id}/command", "post", json={"command": command}
-        )["data"]
-        return _data
-
-    def get_player_lists(self, id: str) -> list:
-        """Get a list of available playerlists
-
-        Args:
-            ``id`` (``str``): The ID of the server
-
-        Returns:
-            ``list``: The List of available playerlists (whitelist, ops, etc.)
-        """
-        _data = self._make_request(f"servers/{id}/playerlists")["data"]
-        return _data
-
-    def get_player_list(self, id: str, player_list: str) -> list:
+    def get_player_list(self) -> list:
         """Get a specific playerlist
 
         Args:
@@ -191,41 +95,5 @@ class Exaroton:
         Returns:
             ``list``: List of players on that list
         """
-        _data = self._make_request(f"servers/{id}/playerlists/{player_list}")["data"]
-        return _data
-
-    def add_player_to_list(self, id: str, player_list: str, usernames: list) -> list:
-        """Add playernames to a playerlist
-
-        Args:
-            id (``str``): The ID of the server
-            player_list (``str``): The name of the playerlist (eg "whitelist")
-            usernames (``list`` | ``str``): The username or multiple thereof to add
-
-        Returns:
-            ``list``: The new list of players on that list
-        """
-        _data = self._make_request(
-            f"servers/{id}/playerlists/{player_list}",
-            "put",
-            json={"entries": usernames},
-        )["data"]
-        return _data
-
-    def remove_player_from_list(self, id: str, player_list: str, usernames: list):
-        """Remove players from a playerlist
-
-        Args:
-            ``id`` (``str``): The ID of the server
-            ``player_list`` (``str``): The name of the playerlist (eg "whitelist")
-            ``usernames`` (``list`` | ``str``): The username of multiple thereof to remove
-
-        Returns:
-            ``list``: The new list of players on that list
-        """
-        _data = self._make_request(
-            f"servers/{id}/playerlists/{player_list}",
-            "delete",
-            json={"entries": usernames},
-        )["data"]
+        _data = self._make_request(f"servers/Pn8J6Yy0TWUqflP6/playerlists/whitelist")["data"]
         return _data
